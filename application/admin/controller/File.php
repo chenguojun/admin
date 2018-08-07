0a4fa55dd832404ad56d32288b9da30b2d0a520c
chenguojun <807368295@qq.com>
2018-08-07 20:35:12 +0800

    增加了照片格式验证

--- application/admin/controller/File.php
+++ application/admin/controller/File.php
@@ -8,12 +8,11 @@ class File{
         // 获取表单上传文件 例如上传了001.jpg
         $file = request()->file('image');
         // 移动到框架应用根目录/public/uploads/ 目录下
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        $info = $file->validate(['size'=>15678,'ext'=>'jpg,png,gif,jpge,bmp'])->move(ROOT_PATH . 'public' . DS . 'uploads');
         if($info){
             echo json_encode(["status"=>1,"savename"=> '/public/uploads/' .date("Ymd")."/".$info->getFilename(),"filename"=>$info->getFilename()]);
         }else{
             echo json_encode(["status"=>0,"reason"=>$file->getError()]);
-
         }
     }
 }
